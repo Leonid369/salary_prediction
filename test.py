9f8fd6f566d82e3899ad6e29918278547c9d49a6
Leonid369 <sara.moorthy369@gmail.com>
2020-12-27 00:59:46 -0600

    added base files

---
+++ test.py
@@ -0,0 +1,19 @@
+import gs_scrapper as gs
+import pandas as pd
+import pickle
+
+
+path = '/Users/saraswathishanmugamoorthy/Documents/chromedriver'
+'''
+df = gs.get_jobs("data scientist", 800, False,path,15 )
+print(df)
+
+pickle_out = open("df.pickle", "wb")
+pickle.dump(df,pickle_out)
+pickle_out.close()
+'''
+
+pickle_in = open("df.pickle", "rb")
+df1 = pickle.load(pickle_in)
+df1 = pd.DataFrame(df1)
+print(df1.head())
